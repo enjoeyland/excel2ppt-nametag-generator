881afb78c468bac47cd14a0a059280ecea7faa05
Min KyungHyun <khmin1104@gmail.com>
2024-02-10 22:43:59 +0900

    feat: get positional content information from slide
    return by dictiary form. relative postion are given based on lefttop shape.

---
+++ analyze_slide.py
@@ -0,0 +1,60 @@
+from pptx import Presentation
+from pptx.enum.shapes import PP_PLACEHOLDER
+from pptx.enum.text import PP_ALIGN
+
+from utils import dotdict
+
+def get_image_info(shape):
+    if shape.is_placeholder and  shape.placeholder_format.type == PP_PLACEHOLDER.PICTURE:
+        return dotdict({
+            "type": "image", 
+            "left": shape.left.cm,
+            "top": shape.top.cm,
+            "width" : shape.width.cm,
+            "height": shape.height.cm,
+            "image": shape.image
+        })
+
+def get_textbox_info(shape):
+    if shape.has_text_frame:
+        return dotdict({
+            "type": "text", 
+            "left": shape.left.cm,
+            "top": shape.top.cm,
+            "width" : shape.width.cm,
+            "height": shape.height.cm,
+            "text": shape.text_frame.text,
+            "alignment": shape.text_frame.paragraphs[0].alignment or PP_ALIGN.LEFT,
+            "font": shape.text_frame.paragraphs[0].runs[0].font
+        })
+
+def to_relative_info(absolute_info):
+    result = dotdict({
+        "type": "layout",
+        "component": []
+    })
+    basis =  absolute_info[0]
+    for info in absolute_info:
+        if info.left < basis.left and info.top < basis.top:
+            basis =  absolute_info[0]
+
+    for info in absolute_info:
+        info.left -= basis.left
+        info.top -= basis.top
+        result.component.append(info)
+    return result
+
+def get_slide_info(slide):
+    absolute_info = []
+    for shape in slide.shapes:
+        print("shape:", shape.name, shape.shape_type)
+        absolute_info.append(get_image_info(shape) or get_textbox_info(shape))
+    return to_relative_info(absolute_info)
+
+if __name__ == "__main__":
+    pptx_filename = 'example/nametag-example.pptx' 
+    prs = Presentation(pptx_filename)
+    
+    for i, slide in enumerate(prs.slides):
+        print(f"{'='*5}#{i} slide{'='*5}")
+        print(get_slide_info(slide))
