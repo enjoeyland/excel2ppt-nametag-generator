230074087de659ed1fdad721d1eded03940251e2
Min KyungHyun <khmin1104@gmail.com>
2024-02-11 01:49:51 +0900

    feat: generalize main code using slide sample info

--- main.py
+++ main.py
@@ -1,98 +1,101 @@
+from io import BytesIO
+from os import path
 from openpyxl import load_workbook
 from pptx import Presentation
 from pptx.util import Cm, Pt
 from pptx.enum.text import PP_ALIGN
 from pptx.dml.color import RGBColor
 
+import settable
+from analyze_slide import get_slide_info
+from utils import chunk_list, tuples_to_dict_list
+
+
+
+class NameTag:
+    def __init__(self, pptx):
+        self._filename = path.basename(pptx)
+        self.prs = Presentation(pptx)
+        self.samples = []
+        for slide in self.prs.slides:
+            self.samples.append(get_slide_info(slide))
+        self.sample_info = self.samples[0]
+        self.num_col, self.num_row = self.get_max_col_row()
+        self.num_per_slide = self.num_col * self.num_row
+        self.left, self.top = self.get_start_pos()
+
+    def get_max_col_row(self):
+        num_col = self.prs.slide_width.cm // self.sample_info.width
+        num_row = self.prs.slide_height.cm // self.sample_info.height
+        return (int(num_col), int(num_row))
+
+    def get_start_pos(self):
+        left = (self.prs.slide_width.cm - self.sample_info.width * self.num_col) / 2
+        top = (self.prs.slide_height.cm - self.sample_info.height * self.num_row) / 2
+        return (left, top)
+
+    def add_nametag_info(self, slide, data):
+        for i, d in enumerate(data):
+            if i == self.num_per_slide:
+                break
+            col_idx = i % 2
+            row_idx = i // 2
+            
+            nametag_form = self.samples[d["sample num"]]
+            for image_form in nametag_form.images:
+                slide.shapes.add_picture(
+                    BytesIO(image_form.image.blob),
+                    Cm(self.left + col_idx * nametag_form.width + image_form.left),
+                    Cm(self.top + row_idx * nametag_form.height + image_form.top),
+                    Cm(image_form.width),
+                    Cm(image_form.height))
+
+            for text_form in nametag_form.texts:
+                p = slide.shapes.add_textbox(
+                    Cm(self.left + col_idx * nametag_form.width + text_form.left),
+                    Cm(self.top + row_idx * nametag_form.height + text_form.top),
+                    Cm(text_form.width),
+                    Cm(text_form.height)
+                ).text_frame.paragraphs[0]
+                p.alignment = text_form.alignment
+                r = p.add_run()
+                r.font = text_form.font
+                r.text = d[text_form.text]
+                
+    def save(self):
+        self.prs.save(f'dist/generated-{self._filename}') 
+
 def read_excel_data(filename):
-    workbook = load_workbook(filename)
+    workbook = load_workbook(filename, data_only=True)
     sheet = workbook.active
     data = []
     for row in sheet.iter_rows(values_only=True):
         data.append(row)
     return data[0], data[1:]
 
+excel_filename = 'example/attendees_list-example.xlsx'  # 엑셀 파일명 입력
+header, nametag_data = read_excel_data(excel_filename)
 
-def add_badge_info(slide, nametag_info, size):
-    for i, shape in enumerate(slide.placeholders):
-        placeholder = slide.placeholders[shape.placeholder_format.idx]
-        picture = placeholder.insert_picture(nametag_info[i % len(nametag_info)][3])
-        
-        if size:
-            new_width, new_height = size
-            picture.left, picture.top = (picture.left + (1 - i % 2) * (picture.width - new_width), picture.top + (1 - i // 2) * (picture.height - new_height))
-            picture.width = new_width
-            picture.height = new_height
+try:
+    sample_num_idx = header.index("sample num")
+except:
+    header += ("sample num",)
+    nametag_data = [d + (0,) for d in nametag_data]
+else:
+    for i, d in enumerate(nametag_data):
+        d = list(d)
+        d[sample_num_idx] = int(d[sample_num_idx])
+        nametag_data[i] = tuple(d)
 
-    left = Cm(0.525)
-    top = Cm(1.4)
-    col_width, row_height = size
-    
-    for i, (캠퍼스, 이름, 순장, _, color) in enumerate(nametag_info):
-        col_idx = i % 2
-        row_idx = i // 2
-        
-        left_offset = col_idx * col_width
-        top_offset = row_idx * row_height 
-        
-        text_left_offset = 0
-        text_top_offset = Cm(4.55) - top
-        textbox = slide.shapes.add_textbox(left + left_offset + text_left_offset, top + top_offset + text_top_offset, col_width, Pt(40))
-        text_frame = textbox.text_frame
-        p = text_frame.paragraphs[0]
-        p.alignment = PP_ALIGN.CENTER
-        run_이름 = p.add_run()
-        run_이름.text = 이름
-        run_이름.font.name = '조선신명조'
-        run_이름.font.size = Pt(32)
+nametag_data = tuples_to_dict_list(header, nametag_data)
 
-        text_left_offset = Cm(0.91) - left
-        text_top_offset = Cm(11.86) - top
-        textbox = slide.shapes.add_textbox(left + left_offset + text_left_offset, top + top_offset + text_top_offset, col_width, Pt(18))
-        text_frame = textbox.text_frame
-        p = text_frame.paragraphs[0]
-        run_캠퍼스_순장 = p.add_run()
-        run_캠퍼스_순장.text = f'{캠퍼스} {순장}'
-        run_캠퍼스_순장.font.name = '조선신명조'
-        run_캠퍼스_순장.font.size = Pt(16)
-        run_캠퍼스_순장.font.color.rgb = color
-        
+pptx_filename = 'example/nametag-example.pptx' 
+nametag = NameTag(pptx_filename)
+nametag_data = chunk_list(nametag_data, nametag.num_per_slide)
 
-def main():
-    # 엑셀 파일에서 데이터 읽기
-    excel_filename = '순리캠 명단.xlsx'  # 엑셀 파일명 입력
-    header, nametag_data = read_excel_data(excel_filename)
+slide_layout = nametag.prs.slide_layouts[0]
+for d in nametag_data:
+    slide = nametag.prs.slides.add_slide(slide_layout)
+    nametag.add_nametag_info(slide, d)
 
-    image_path = {
-        '간사': '순리캠 명찰_간사.png',
-        '순장': '순리캠 명찰_순장.png',
-        '예비순장': '순리캠 명찰_예비순장.png',
-    }
-    main_color = {
-        '간사': RGBColor(22, 46, 119),
-        '순장': RGBColor(25, 69, 37),
-        '예비순장': RGBColor(196, 63, 29),
-    }
-    nametag_data = [(*nt, image_path[nt[2]], main_color[nt[2]]) for nt in nametag_data]
-
-    pptx_filename = '순리캠 명찰.pptx' 
-    prs = Presentation(pptx_filename)
-
-
-    nametag_size = (Cm(9.0), Cm(11.8))
-
-    num_columns = 2
-    num_rows = 2
-    nametag_per_slide = num_columns * num_rows
-    for i in range(0, len(nametag_data), nametag_per_slide):
-        nametag_info = nametag_data[i:i+nametag_per_slide]
-
-        slide_layout = prs.slide_layouts[0]
-        slide = prs.slides.add_slide(slide_layout)
-        
-        add_badge_info(slide, nametag_info, nametag_size)
-    
-    prs.save('생성된 순리캠 명찰.pptx')  # 출력 파일명 입력
-
-if __name__ == "__main__":
-    main()
+nametag.save()
--- utils.py
+++ utils.py
@@ -5,3 +5,9 @@ class dotdict(dict):
     __delattr__ = dict.__delitem__
     def copy(self):
         return dotdict(super().copy())
+
+def chunk_list(l, chunk_size):
+    return [l[i:i + chunk_size] for i in range(0, len(l), chunk_size)]
+
+def tuples_to_dict_list(header, data):
+    return [dict(zip(header, d)) for d in data]
