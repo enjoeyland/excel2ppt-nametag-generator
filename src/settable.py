a3fc83065453f2aa6460d2b35aa261239b2b4eff
Min KyungHyun <khmin1104@gmail.com>
2024-02-11 18:16:07 +0900

    refactor: simplify code which adds setter to _Run.font by monkey patching

--- src/settable.py
+++ src/settable.py
@@ -1,67 +1,16 @@
-from pptx.text import text
+from pptx.text.text import _Run
 
-class _SettableRun(text.Subshape):
-    """Text run object. Corresponds to ``<a:r>`` child element in a paragraph."""
+def font(self, font): # monkey patch
+    self.font.bold = font.bold
+    self.font.italic = font.italic
+    self.font.language_id = font.language_id
+    self.font.name = font.name
+    self.font.size = font.size
+    self.font.underline = font.underline
+    if hasattr(font.color, 'rgb'):
+        self.font.color.rgb = font.color.rgb
 
-    def __init__(self, r, parent):
-        super(_SettableRun, self).__init__(parent)
-        self._r = r
-
-    @property
-    def font(self):
-        """
-        |Font| instance containing run-level character properties for the
-        text in this run. Character properties can be and perhaps most often
-        are inherited from parent objects such as the paragraph and slide
-        layout the run is contained in. Only those specifically overridden at
-        the run level are contained in the font object.
-        """
-        rPr = self._r.get_or_add_rPr()
-        return text.Font(rPr)
-
-    @font.setter
-    def font(self, font):
-        self.font.bold = font.bold
-        self.font.italic = font.italic
-        self.font.language_id = font.language_id
-        self.font.name = font.name
-        self.font.size = font.size
-        self.font.underline = font.underline
-        if hasattr(font.color, 'rgb'):
-            self.font.color.rgb = font.color.rgb
-
-    @text.lazyproperty
-    def hyperlink(self):
-        """
-        |_Hyperlink| instance acting as proxy for any ``<a:hlinkClick>``
-        element under the run properties element. Created on demand, the
-        hyperlink object is available whether an ``<a:hlinkClick>`` element
-        is present or not, and creates or deletes that element as appropriate
-        in response to actions on its methods and attributes.
-        """
-        rPr = self._r.get_or_add_rPr()
-        return text._Hyperlink(rPr, self)
-
-    @property
-    def text(self):
-        """Read/write. A unicode string containing the text in this run.
-
-        Assignment replaces all text in the run. The assigned value can be a 7-bit ASCII
-        string, a UTF-8 encoded 8-bit string, or unicode. String values are converted to
-        unicode assuming UTF-8 encoding.
-
-        Any other control characters in the assigned string other than tab or newline
-        are escaped as a hex representation. For example, ESC (ASCII 27) is escaped as
-        "_x001B_". Contrast the behavior of `TextFrame.text` and `_Paragraph.text` with
-        respect to line-feed and vertical-tab characters.
-        """
-        return self._r.text
-
-    @text.setter
-    def text(self, str):
-        self._r.text = text.to_unicode(str)
-
-text._Run = _SettableRun
+_Run.font = _Run.font.setter(font)
 
 if __name__ == "__main__":
     from pptx import Presentation
